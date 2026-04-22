55c330a40e70ca10fcd37f88fc48aa2efc7f7ec0
ThamiduChamod <thamiduchamod748@gmail.com>
2026-04-22 12:29:49 +0530

    implement user repository

---
+++ src/main/java/lk/privat_coaching_backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package lk.privat_coaching_backend.repository;
+
+import lk.privat_coaching_backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
