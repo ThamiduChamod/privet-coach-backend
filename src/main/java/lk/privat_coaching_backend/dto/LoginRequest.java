d783f85b52c01840356597569e6ffdfcc2d0fa32
ThamiduChamod <thamiduchamod748@gmail.com>
2026-04-22 12:28:21 +0530

    implement login DTO

---
+++ src/main/java/lk/privat_coaching_backend/dto/LoginRequest.java
@@ -0,0 +1,22 @@
+package lk.privat_coaching_backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+
+}
