8eb33b8889fd77e26d846460238dadd87b270983
ThamiduChamod <thamiduchamod748@gmail.com>
2026-04-22 12:25:00 +0530

    implement spring security (cross police)

---
+++ src/main/java/lk/privat_coaching_backend/configeration/CorsConfig.java
@@ -0,0 +1,42 @@
+package lk.privat_coaching_backend.configeration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        {
+            http
+                    .cors(cors -> cors.configurationSource(request -> {
+                        CorsConfiguration config = new CorsConfiguration();
+
+                        config.setAllowedOrigins(List.of("http://localhost:3000"));
+                        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                        config.setAllowedHeaders(List.of("*"));
+                        config.setAllowCredentials(true);
+
+                        return config;
+                    }))
+                    .csrf(csrf -> csrf.disable())
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/api/auth/**").permitAll()
+                                    .anyRequest().authenticated()
+                    )
+                    .oauth2Login(oauth -> {})
+                    .httpBasic(httpBasic -> {});
+            return http.build();
+
+        }
+    }
+}
