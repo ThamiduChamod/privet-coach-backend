df23943d8af2501c7204e34149dfc7042b84c8f2
ThamiduChamod <thamiduchamod748@gmail.com>
2026-04-22 12:29:36 +0530

    implement user entity

---
+++ src/main/java/lk/privat_coaching_backend/PrivatCoachingBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.privat_coaching_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivatCoachingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivatCoachingBackendApplication.class, args);
+    }
+
+}
